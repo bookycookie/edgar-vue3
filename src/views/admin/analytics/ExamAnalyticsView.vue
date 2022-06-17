19224b004adfca4d532be14a9e3cff7ab233016f
Borna Goljacki <borna.goljacki@gmail.com>
2022-06-17 07:27:19 +0200

    Start implementing analytics

--- src/views/admin/analytics/ExamAnalyticsView.vue
+++ src/views/admin/analytics/ExamAnalyticsView.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as d3 from 'd3';
+import { TestsSingle } from '@/models/admin/analytics/TestsSingle';
+import ApiService from '@/services/ApiService';
+
+const service = new ApiService();
+
+const courseId = 155;
+const academicYearId = 2020;
+
 const boilerplate = '';
+const tests = ref<TestsSingle[]>([]);
+
+onMounted(async () => {
+	await service
+		.getManyAsync<TestsSingle>('/tests/single', {
+			courseId: courseId,
+			academicYearId: academicYearId,
+		})
+		.then((testsSingle: TestsSingle[]) => {
+			tests.value = testsSingle;
+		});
+});
 </script>
 
-<template><div>boilerplate</div></template>
+<template>
+	<div class="container-fluid">
+		<Card>
+			<template #title>Exam analytics (click on the boxplot to see details)</template>
+			<template #content>{{ tests }}</template>
+		</Card>
+	</div>
+</template>
